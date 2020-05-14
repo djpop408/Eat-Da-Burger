5202e933cf11e8d3436866b604a1b8170a4b34a1
Anthony Nguyen <anthonynguyen@Anthonys-MBP.attlocal.net>
2020-05-14 14:32:14 -0700

    pushing latest code for burger assignment

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// Mysql data connection
+
+var mysql = require('mysql');
+require('dotenv').config();
+var connection; 
+
+// connection to the DB
+if(process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+}else{  
+connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'burgers_db'
+});
+};
+
+// testing the connection
+connection.connect(function(err){
+  if(err)throw err;
+  console.log("connected as id: " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ database/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', TRUE);
