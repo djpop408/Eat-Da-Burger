d6556889e71ec6b3e3633ffc3c45b0a20cb2552a
Anthony Nguyen <anthonynguyen@Anthonys-MBP.attlocal.net>
2020-05-15 00:05:08 -0700

    updated some connection stuff

--- config/connection.js
+++ config/connection.js
@@ -7,22 +7,23 @@ var connection;
 // connection to the DB
 if(process.env.JAWSDB_URL){
   connection=mysql.createConnection(process.env.JAWSDB_URL);
-}else{  
+} else {  
 connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: 'burgers_db'
-});
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'burgers_db'
+  });
 };
 
-// testing the connection
-// connection.connect(function(err){
-//   if(err)throw err;
-//   console.log("connected as id: " + connection.threadId);
-// });
+connection.connect(function(err) {
+  if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
 
-connection.connect();
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require ('method-override');
 var exphbs = require('express-handlebars');
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 
 var app = express();
 app.use(express.static('public'));
@@ -10,10 +14,6 @@ app.use(bodyParser.urlencoded({
   extended: false
 }))
 
-app.use(methodOverride('_method'));
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
 app.set('view engine','handlebars');
 
 var routes = require('./controllers/routes.js');
