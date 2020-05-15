fe16e698d29f28e2ee1b0b01b37e82bc1a198ef2
Anthony Nguyen <anthonynguyen@Anthonys-MBP.attlocal.net>
2020-05-15 00:15:03 -0700

    pushing latest server.js updates

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require ('method-override');
 var exphbs = require('express-handlebars');
-app.use(methodOverride('_method'));
+var timeout = require('connect-timeout');
+var morgan = require('morgan');
+var fs = require('fs');
+var path = require('path');
+
+var app = express();
+app.use('/static', express.static('public'));
+app.use(timeout(15000));
+
+//Sets up morgan for logging
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+// setup the logger (writes to the file)
+app.use(morgan('combined', { stream: accessLogStream }));
+
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 
-var app = express();
-app.use(express.static('public'));
-
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.set('view engine','handlebars');
 
